dd968adde3accb4387f6dcb00cd1ea5a4b612b61
Juan Melo <juan.melo-s@escuelaing.edu.co>
2026-02-05 16:00:49 -0500

    chore: inicio reto 5 archivo base

--- src/Laboratorio1/reto5/reto5.java
+++ src/Laboratorio1/reto5/reto5.java
@@ -1,8 +1,7 @@
 package Laboratorio1.reto5;
 
-public class reto5 {
+public class Reto5 {
     public static void main(String[] args) {
         System.out.println("--- BATALLA DE CONJUNTOS ---");
     }
 }
-
