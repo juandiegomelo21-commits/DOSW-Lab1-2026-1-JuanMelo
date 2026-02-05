d1de8c96f353006ea0d9bcb51a384de3a6848b8f
Juan Melo <juan.melo-s@escuelaing.edu.co>
2026-02-05 14:11:02 -0500

    feat(carril1): añadida funcion procesarLista parcial

--- src/Laboratorio1/reto2/CarreraParalela.java
+++ src/Laboratorio1/reto2/CarreraParalela.java
@@ -17,4 +17,23 @@ public class CarreraParalela {
                 .orElse(0); // Retorna 0 si vacía
         return buscarMax.apply(numeros);
     }
+
+    // Clase auxiliar para guardar resultados
+    static class Resultados {
+        int max;
+        int min; // Este campo lo llenaremos después
+        long cantidad;
+        public Resultados(int max, int min, long cantidad) {
+            this.max = max;
+            this.min = min;
+            this.cantidad = cantidad;
+        }
+    }
+
+    // FUNCION CONFLICTIVA - VERSION CARRIL 1
+    public static Resultados procesarLista(List<Integer> numeros) {
+        int max = obtenerMaximo(numeros);
+        // Como soy el Carril 1, aún no sé calcular el mínimo, pongo 0
+        return new Resultados(max, 0, numeros.size());
+    }
 }
