54d90645daf29651afae0f155f5ac472be9051b7
Juan Melo <juan.melo-s@escuelaing.edu.co>
2026-02-05 15:16:32 -0500

    feat(builder): eco repetitivo con stringbuilder

--- src/Laboratorio1/reto3/reto3.java
+++ src/Laboratorio1/reto3/reto3.java
@@ -5,4 +5,15 @@ public class reto3 {
         System.out.println("--- ECO MISTERIOSO ---");
         // Aquí probaremos el eco
     }
+
+
+    // Lógica del Estudiante A: Repetir 3 veces con StringBuilder
+    public static String generarEco(String mensaje) {
+        StringBuilder sb = new StringBuilder();
+        // Simulación simple del builder
+        sb.append(mensaje).append(" ")
+                .append(mensaje).append(" ")
+                .append(mensaje);
+        return sb.toString();
+    }
 }
