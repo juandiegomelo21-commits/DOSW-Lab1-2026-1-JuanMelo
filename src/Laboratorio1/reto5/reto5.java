3ed0d2b16d4e028d8007a5ba50ec8f00c2438626
Juan Melo <juan.melo-s@escuelaing.edu.co>
2026-02-05 16:02:44 -0500

    feat(hash): implementacion hashset eliminando multiplos de 3

--- src/Laboratorio1/reto5/reto5.java
+++ src/Laboratorio1/reto5/reto5.java
@@ -1,7 +1,16 @@
 package Laboratorio1.reto5;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Reto5 {
+public class reto5 {
     public static void main(String[] args) {
         System.out.println("--- BATALLA DE CONJUNTOS ---");
+    }
+    // Lógica Estudiante A: HashSet (Desordenado) - Elimina múltiplos de 3
+    public static Set<Integer> crearEjercitoHash(List<Integer> numeros) {
+        Set<Integer> hashSet = new HashSet<>(numeros);
+        // Regla: Eliminar múltiplos de 3
+        hashSet.removeIf(n -> n % 3 == 0);
+        return hashSet;
     }
 }
