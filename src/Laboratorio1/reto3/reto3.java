13bb8bcd4e8705f65248c3f3ba8e0880ddd405c5
Juan Melo <juan.melo-s@escuelaing.edu.co>
2026-02-05 15:15:44 -0500

    chore: inicio reto 3 archivo base

--- src/Laboratorio1/reto3/reto3.java
+++ src/Laboratorio1/reto3/reto3.java
@@ -1,4 +1,8 @@
 package Laboratorio1.reto3;
 
 public class reto3 {
+    public static void main(String[] args) {
+        System.out.println("--- ECO MISTERIOSO ---");
+        // Aquí probaremos el eco
+    }
 }
