6f500508092d951164dc3b7cc506b884cba35440
Juan Melo <juan.melo-s@escuelaing.edu.co>
2026-02-05 16:32:04 -0500

    feat(switch): comandos basicos seccion A

--- src/Laboratorio1/reto6/reto6.java
+++ src/Laboratorio1/reto6/reto6.java
@@ -3,5 +3,26 @@ package Laboratorio1.reto6;
 public class reto6 {
     public static void main(String[] args) {
         System.out.println("--- MÁQUINA DE DECISIONES ---");
+        procesarComando("SALUDAR");
+    }
+
+    // Lógica Estudiante A: Switch con sus comandos
+    public static void procesarComando(String comando) {
+        switch (comando) {
+            case "SALUDAR":
+                System.out.println("¡Saludos, viajero del tiempo y del código!");
+                break;
+            case "DESPEDIR":
+                System.out.println("Que los bits te acompañen, hasta la próxima misión.");
+                break;
+            case "CANTAR":
+                System.out.println("01010101 🎵");
+                break;
+            case "DANZAR":
+                System.out.println("Girando en modo fiesta 💃");
+                break;
+            default:
+                System.out.println("Error: Comando no reconocido por la Sección A.");
+        }
     }
 }
